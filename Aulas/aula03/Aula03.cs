d063139b99e714f8382ac141c455ad2d77fbae10
JG-OLIVEIRA <jorge.gdoliveira8@gmail.com>
2023-04-20 14:06:09 -0300

    Introduzing variables

---
+++ Aulas/aula03/Aula03.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Aula03 {
+    static void Main(){
+        int num1, num2;
+
+        num1=10;
+        num2=2;
+
+        Console.WriteLine("O valor da multiplicação de " + num1 + " com " + num2 + " é igual a " + num1*num2);
+    }
+}
