cdc8662bf577342e2f9e3e1256a765827f7b25b5
JG-OLIVEIRA <jorge.gdoliveira8@gmail.com>
2023-04-20 10:24:43 -0300

    The first 'Hello Word'

---
+++ Curso_cs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Curso_cs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
