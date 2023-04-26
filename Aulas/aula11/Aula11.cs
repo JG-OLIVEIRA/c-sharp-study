c4035026634e466fb8ee5da3cc3742ae0e927e79
JG-OLIVEIRA <jorge.gdoliveira8@gmail.com>
2023-04-26 08:38:02 -0300

    Explict and implict type casts

---
+++ Aulas/aula11/Aula11.cs
@@ -0,0 +1,17 @@
+using System;
+
+class aula11{
+    static void Main(){
+
+        // Conversões implícitas
+        int vInt = 10;
+        float vFloat = vInt;
+
+        Console.WriteLine(vShort);
+
+        // Conversões explícitas
+        short vShort=(short)vInt;
+
+        Console.WriteLine(vShort);
+    }
+}
