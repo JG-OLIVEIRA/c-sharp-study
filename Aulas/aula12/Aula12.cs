9d3bd0e1fa7de1dc80dd8fb1d7a6e8107dae1e96
JG-OLIVEIRA <jorge.gdoliveira8@gmail.com>
2023-04-26 09:32:40 -0300

    Conditional IF

---
+++ Aulas/aula12/Aula12.cs
@@ -0,0 +1,24 @@
+using System;
+
+class Aula12{
+    static void Main(){
+
+        int p1, p2, res;
+        p1=p2=res=0;
+        string resultado="Reprovado";
+
+        Console.Write("Digite a P1: ");
+        p1=int.Parse(Console.ReadLine());
+        
+        Console.Write("Digite a P2: ");
+        p2=int.Parse(Console.ReadLine());
+
+        res=p1+p2;
+
+        if(res>=120){
+            resultado="Aprovado";
+        }
+
+        Console.WriteLine("Resultado: {0}", resultado);
+    }
+}
