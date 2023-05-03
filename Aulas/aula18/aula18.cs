8fec1ccc590fd6138c5d2ca9967b1d7225ce7a1f
JG-OLIVEIRA <jorge.gdoliveira8@gmail.com>
2023-05-03 09:10:52 -0300

    Loop for

---
+++ Aulas/aula18/aula18.cs
@@ -0,0 +1,22 @@
+using System;
+
+class Aula18{
+    static void Main(){
+        for(int num=0;num<10;num++){
+            Console.WriteLine("CFB CURSOS {0}", num);
+        }
+
+        Console.WriteLine("");
+
+        int[] numbers = new int[10];
+
+        for(int i=0;i<numbers.Length;i++){
+            numbers[i] = 2 * i;
+        }
+
+        for(int i=0;i<numbers.Length;i++){
+            Console.WriteLine("Valor: {0}", numbers[i]);
+        }
+
+    }
+}
