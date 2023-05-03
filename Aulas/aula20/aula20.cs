0ae97c4ea42f2609075127683be850db8e9357ec
JG-OLIVEIRA <jorge.gdoliveira8@gmail.com>
2023-05-03 11:40:04 -0300

    Loop DO-WHILE

---
+++ Aulas/aula20/aula20.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Aula20{
+    static void Main(){
+
+        string password="passwordaula20";
+        string checkPassword = "";
+        int attempts = 0;
+
+        do{
+            Console.Clear();
+            Console.Write("Digite a senha: ");
+            checkPassword = Console.ReadLine();
+            attempts++;
+        }while(password!=checkPassword);
+
+        Console.WriteLine("Senha correta! Número de tentativas: {0}", attempts);
+
+    }
+}
