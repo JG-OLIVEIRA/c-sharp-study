6aa584855d5e73e2548284357e490df7d54ca80b
JG-OLIVEIRA <jorge.gdoliveira8@gmail.com>
2023-05-04 11:43:21 -0300

    The ref keyword in params methods

---
+++ Aulas/aula24/aula24.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Aula24{
+    static void Main(){
+        int num=10;
+        Double(num);
+        Console.WriteLine(num);
+    }
+
+    static void Double(int value){
+        value*=2;
+    }
+
+    static void Double(ref int value){
+        value*=2;
+    }
+}
